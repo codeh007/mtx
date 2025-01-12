68d51f75b982aca565324d5bb2a6e07c2497ab12
a <a@a.com>
2025-01-12 12:28:32 +0000

    1

--- packages/mtmaiui/src/app/(web)/page.tsx
+++ packages/mtmaiui/src/app/(web)/page.tsx
@@ -1,21 +1,13 @@
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { getQueryClient } from "mtmaiui/stores/get-query-client";
 import { MtSuspenseBoundary } from "mtxuilib/components/MtSuspenseBoundary";
-// import { HelloXLz } from "../../componenets/HelloXLz";
-// import { useSuspenseQuery } from "@tanstack/react-query";
-// import { postListOptions } from "mtmaiapi";
 import { HelloXLz } from "../../components/HelloXLz";
 
 export default async function Page(props: { params }) {
   const queryClient = getQueryClient();
-
-  // const postQuery = useSuspenseQuery({
-  //   ...postListOptions({}),
-  // });
   return (
     <>
       <HelloXLz />
-      aaaaa page
       <HydrationBoundary state={dehydrate(queryClient)}>
         <MtSuspenseBoundary>
           {/* <PokemonInfo /> */}
