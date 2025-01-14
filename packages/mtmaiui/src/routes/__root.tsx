ba9f835d7c5e44e4e5f1d6fa0d783b9d00d37846
a <a.com>
2025-01-14 12:49:29 +0800

    1

--- packages/mtmaiui/src/routes/__root.tsx
+++ packages/mtmaiui/src/routes/__root.tsx
@@ -9,7 +9,7 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <div className="fixed flex top-0 left-0 w-full h-full bg-blue-400">
+    <div className="fixed flex top-0 left-0 w-full h-full">
       <UserFAB />
       <Outlet />
     </div>
