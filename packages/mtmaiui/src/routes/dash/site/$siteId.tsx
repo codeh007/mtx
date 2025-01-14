9951fd17fa8f172b932400d2ef6bb90018f0398e
a <a.com>
2025-01-14 15:17:41 +0800

    1

--- packages/mtmaiui/src/routes/dash/site/$siteId.tsx
+++ packages/mtmaiui/src/routes/dash/site/$siteId.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { SiteEditor } from "../../../components/site/SiteEditor";
+import { useTenant } from "../../../hooks/useAuth";
 
 export const Route = createFileRoute("/dash/site/$siteId")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/dash/site/$siteId"!</div>;
+  const { siteId } = Route.useParams();
+  const tenant = useTenant();
+  if (!tenant) {
+    return null;
+  }
+  return (
+    <div>
+      <SiteEditor siteId={siteId} />
+    </div>
+  );
 }
