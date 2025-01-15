0dbd45c610ac93d67b1984cbafd54cfe8644bf38
a <a.com>
2025-01-15 12:34:56 +0800

    1

--- packages/mtmaiui/src/routes/dash/workflow-runs/$workflowRunId.tsx
+++ packages/mtmaiui/src/routes/dash/workflow-runs/$workflowRunId.tsx
@@ -1,9 +1,159 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { CodeHighlighter } from "mtxuilib/mt/code-highlighter";
+import {
+  MtTabs,
+  MtTabsContent,
+  MtTabsList,
+  MtTabsTrigger,
+} from "mtxuilib/mt/tabs";
+import { Separator } from "mtxuilib/ui/separator";
+import { Sheet, SheetContent } from "mtxuilib/ui/sheet";
+import { useEffect, useState } from "react";
+import RunDetailHeader from "../../../components/workflow-run/header";
+import { MiniMap } from "../../../components/workflow-run/mini-map";
+import {
+  StepRunDetail,
+  TabOption,
+} from "../../../components/workflow-run/step-run-detail/step-run-detail";
+import { StepRunEvents } from "../../../components/workflow-run/step-run-events-for-workflow-run";
+import {
+  ViewToggle,
+  hasChildSteps,
+} from "../../../components/workflow-run/view-toggle";
+import { WorkflowRunInputDialog } from "../../../components/workflow-run/workflow-run-input";
+import WorkflowRunVisualizer from "../../../components/workflow-run/workflow-run-visualizer-v2";
+import { useTenant } from "../../../hooks/useAuth";
+import { useWorkflowRunShape } from "../../../hooks/useWorkflowRun";
+import { useMtmaiV2 } from "../../../stores/StoreProvider";
+// import { run } from "node:test";
 
 export const Route = createFileRoute("/dash/workflow-runs/$workflowRunId")({
   component: RouteComponent,
 });
-
+interface WorkflowRunSidebarState {
+  workflowRunId?: string;
+  stepRunId?: string;
+  defaultOpenTab?: TabOption;
+}
 function RouteComponent() {
-  return <div>Hello "/dash/workflow-runs/$workflowRunId"!</div>;
+  const tenant = useTenant();
+  if (!tenant) {
+    return <div>require tenant</div>;
+  }
+  const { workflowRunId } = Route.useParams();
+  const [sidebarState, setSidebarState] = useState<WorkflowRunSidebarState>();
+  const { shape } = useWorkflowRunShape(workflowRunId);
+  useEffect(() => {
+    if (
+      sidebarState?.workflowRunId &&
+      workflowRunId !== sidebarState?.workflowRunId
+    ) {
+      setSidebarState(undefined);
+    }
+  }, [workflowRunId, sidebarState]);
+  const view = useMtmaiV2((x) => x.preferredWorkflowRunView);
+
+  return (
+    <div className="flex-grow h-full w-full">
+      <div className="mx-auto max-w-7xl pt-2 px-4 sm:px-6 lg:px-8">
+        <RunDetailHeader
+          loading={shape.isLoading}
+          data={shape.data}
+          refetch={() => shape.refetch()}
+        />
+        <Separator className="my-2" />
+        <div className="w-full h-fit flex overflow-auto relative bg-slate-100 dark:bg-slate-900">
+          {shape.data && view === "graph" && hasChildSteps(shape.data) && (
+            <WorkflowRunVisualizer
+              shape={shape.data}
+              selectedStepRunId={sidebarState?.stepRunId}
+              setSelectedStepRunId={(stepRunId) => {
+                setSidebarState({
+                  stepRunId,
+                  defaultOpenTab: TabOption.Output,
+                  workflowRunId: workflowRunId,
+                });
+              }}
+            />
+          )}
+          {shape.data && (view === "minimap" || !hasChildSteps(shape.data)) && (
+            <MiniMap
+              shape={shape.data}
+              selectedStepRunId={sidebarState?.stepRunId}
+              onClick={(stepRunId, defaultOpenTab?: TabOption) =>
+                setSidebarState(
+                  stepRunId === sidebarState?.stepRunId
+                    ? undefined
+                    : {
+                        stepRunId,
+                        defaultOpenTab,
+                        workflowRunId: workflowRunId,
+                      },
+                )
+              }
+            />
+          )}
+          {shape.data && <ViewToggle shape={shape.data} />}
+        </div>
+        <div className="h-4" />
+        <MtTabs defaultValue="activity">
+          <MtTabsList layout="underlined">
+            <MtTabsTrigger variant="underlined" value="activity">
+              Activity
+            </MtTabsTrigger>
+            <MtTabsTrigger variant="underlined" value="input">
+              Input
+            </MtTabsTrigger>
+            <MtTabsTrigger variant="underlined" value="additional-metadata">
+              Metadata
+            </MtTabsTrigger>
+          </MtTabsList>
+          <MtTabsContent value="activity">
+            <div className="h-4" />
+            {!shape.isLoading && shape.data && (
+              <StepRunEvents
+                workflowRun={shape.data}
+                onClick={(stepRunId) =>
+                  setSidebarState(
+                    stepRunId === sidebarState?.stepRunId
+                      ? undefined
+                      : { stepRunId, workflowRunId: workflowRunId },
+                  )
+                }
+              />
+            )}
+          </MtTabsContent>
+          <MtTabsContent TabsContent value="input">
+            {shape.data && <WorkflowRunInputDialog run={shape.data} />}
+          </MtTabsContent>
+          <MtTabsContent value="additional-metadata">
+            <CodeHighlighter
+              className="my-4"
+              language="json"
+              code={JSON.stringify(shape.data?.additionalMetadata, null, 2)}
+            />
+          </MtTabsContent>
+        </MtTabs>
+      </div>
+      {shape.data && (
+        <Sheet
+          open={!!sidebarState}
+          onOpenChange={(open) =>
+            open ? undefined : setSidebarState(undefined)
+          }
+        >
+          <SheetContent className="w-fit min-w-[56rem] max-w-4xl sm:max-w-2xl z-[60]">
+            {sidebarState?.stepRunId && (
+              <StepRunDetail
+                tenant={tenant}
+                stepRunId={sidebarState?.stepRunId}
+                workflowRun={shape.data}
+                defaultOpenTab={sidebarState?.defaultOpenTab}
+              />
+            )}
+          </SheetContent>
+        </Sheet>
+      )}
+    </div>
+  );
 }
