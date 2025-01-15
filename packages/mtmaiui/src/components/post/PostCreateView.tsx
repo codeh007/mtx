2dbe7e3a1ccca404f3698fffb2ba3a2320e19085
a <a.com>
2025-01-15 09:03:28 +0800

    1

--- packages/mtmaiui/src/components/post/PostCreateView.tsx
+++ packages/mtmaiui/src/components/post/PostCreateView.tsx
@@ -67,7 +67,6 @@ export const PostCreateView = (props: PostCreateViewProps) => {
   });
   return (
     <div>
-      {/* <DebugValue data={post} /> */}
       <ZForm
         form={form}
         handleSubmit={(values) => {
