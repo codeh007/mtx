821aa80b4bcd06ad790051158af4cec32c36bbe6
a <a.com>
2025-01-18 10:32:17 +0800

    1

--- packages/mtmaiui/src/chatbot/ui-messages/DocAdminPanel.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export const DocsAdminPanel = () => {
-	return (
-		<div>
-			<h1>TODO: 知识库管理面板</h1>
-		</div>
-	);
-};
--- packages/mtxlib/src/agents/runnable_config.ts
+++
@@ -1,4 +0,0 @@
-import { Annotation } from "@langchain/langgraph";
-export const ConfigurableAnnotation = Annotation.Root({
-  expectedField: Annotation<string>,
-});
