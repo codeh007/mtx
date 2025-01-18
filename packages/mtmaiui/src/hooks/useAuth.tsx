a630b207a6f764aa6adf292af79084193b2916ad
a <a.com>
2025-01-18 16:21:38 +0800

    1

--- packages/mtmaiui/src/hooks/useAuth.tsx
+++ packages/mtmaiui/src/hooks/useAuth.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { userGetCurrentOptions, userUpdateLoginMutation } from "mtmaiapi";
+import {
+  tenantMembershipsListOptions,
+  userGetCurrentOptions,
+  userUpdateLoginMutation,
+} from "mtmaiapi";
 import { useMtRouter } from "mtxuilib/hooks/use-router";
 import { setCookie } from "mtxuilib/lib/clientlib";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+// import { Route } from "@tanstack/react-router";
+import { Route } from "../routes/__root";
 import { useMtmaiV2 } from "../stores/StoreProvider";
 import { useApiError } from "./useApi";
+
+import { useBasePath } from "./useBasePath";
 
 export const useUser = () => {
   const userQuery = useQuery({
@@ -62,3 +70,78 @@ export const useLoginHandler = () => {
 
   return { loginHandler, isPending: loginMutation.isPending, fieldErrors };
 };
+
+/**
+ * 基本数据的加载
+ * @param props
+ * @returns
+ */
+export function useSessionLoader() {
+  const currentTenant = useMtmaiV2((x) => x.currentTenant);
+  const setCurrentTenant = useMtmaiV2((x) => x.setCurrentTenant);
+  const lastTenant = useMtmaiV2((x) => x.lastTenant);
+
+  const basePath = useBasePath();
+  const listMembershipsQuery = useQuery({
+    ...tenantMembershipsListOptions(),
+  });
+
+  const memberships = useMemo(() => {
+    return listMembershipsQuery.data?.rows || [];
+  }, [listMembershipsQuery]);
+
+  const navigate = Route.useNavigate();
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  const computedCurrTenant = useMemo(() => {
+    const findTenant = (tenantId: string) => {
+      return memberships?.find((m) => m.tenant?.metadata.id === tenantId)
+        ?.tenant;
+    };
+
+    // const currTenantId = searchParams.get("tenant") || undefined;
+
+    // if (currTenantId) {
+    //   const tenant = findTenant(currTenantId);
+
+    //   if (tenant) {
+    //     return tenant;
+    //   }
+    // }
+
+    const lastTenantId = lastTenant?.metadata.id || undefined;
+
+    if (lastTenantId) {
+      const tenant = findTenant(lastTenantId);
+
+      if (tenant) {
+        return tenant;
+      }
+    }
+
+    if (memberships?.length === 0) {
+      // 如果没有任何租户，则创建一个默认租户
+      // const tenant = await api.tenantCreate({
+      //   name: "Default",
+      //   slug: "default",
+      // });
+      // return tenant.data;
+
+      navigate({
+        to: `${basePath}/onboarding/create-tenant`,
+      });
+    }
+    const firstMembershipTenant = memberships?.[0]?.tenant;
+
+    return firstMembershipTenant;
+  }, [basePath, memberships, lastTenant?.metadata.id]);
+
+  useEffect(() => {
+    // 当computedCurrTenant有值，且currentTenant没有值时，设置currentTenant
+    if (computedCurrTenant && !currentTenant) {
+      setCurrentTenant(computedCurrTenant);
+    }
+  }, [computedCurrTenant, currentTenant, setCurrentTenant]);
+
+  return null;
+}
