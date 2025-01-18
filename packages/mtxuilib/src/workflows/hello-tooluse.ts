0548c55d9c24eaade453f51b21a7eb35ecea9cf0
a <ci@gmail.com>
2025-01-18 07:54:02 +0000

    1

--- packages/mtxuilib/src/workflows/hello-tooluse.ts
+++ packages/mtxuilib/src/workflows/hello-tooluse.ts
@@ -4,7 +4,7 @@ import {
   type WorkflowEvent,
   type WorkflowStep,
 } from "cloudflare:workers";
-import { createSimulationGraph } from "../../../mtxlib/src/agents/simulationGraph";
+// import { createSimulationGraph } from "../../../mtxlib/src/agents/simulationGraph";
 import { generateUUID } from "../lib/s-utils";
 import type { Env } from "../types";
 
@@ -20,25 +20,26 @@ export class WfHelloToolUse extends WorkflowEntrypoint<Env, Params> {
       const id = generateUUID();
       const config = { configurable: { thread_id: id } };
 
-      const wf = createSimulationGraph();
-      const graph = wf.compile({ checkpointer });
-      const inputs = {};
-      const contents: string[] = [];
-      for await (const chunk of await graph.stream(inputs, {
-        ...config,
-        // streamMode: "values",
-      })) {
-        const nodeName = Object.keys(chunk)[0];
-        const messages = chunk[nodeName].messages;
-        contents.push(`${nodeName}: ${messages[0].content}`);
-      }
+      // const wf = createSimulationGraph();
+      // const graph = wf.compile({ checkpointer });
+      // const inputs = {};
+      // const contents: string[] = [];
+      // for await (const chunk of await graph.stream(inputs, {
+      //   ...config,
+      //   // streamMode: "values",
+      // })) {
+      //   const nodeName = Object.keys(chunk)[0];
+      //   const messages = chunk[nodeName].messages;
+      //   contents.push(`${nodeName}: ${messages[0].content}`);
+      // }
 
-      console.log(contents.join("\n---\n"));
-      const finnalState = await graph.getState(config);
+      // console.log(contents.join("\n---\n"));
+      // const finnalState = await graph.getState(config);
 
       return {
-        contents,
-        finnalStateValues: finnalState.values,
+        // contents,
+        fake: "fake todo",
+        // finnalStateValues: finnalState.values,
       };
     });
     return {
