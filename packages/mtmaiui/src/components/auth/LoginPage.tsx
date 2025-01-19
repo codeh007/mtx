e0ded1b3c4129b23c85d287628e96c08b703bdd1
ci <a@a.com>
2025-01-19 07:22:32 +0000

    1

--- packages/mtmaiui/src/components/auth/LoginPage.tsx
+++ packages/mtmaiui/src/components/auth/LoginPage.tsx
@@ -6,31 +6,24 @@ import { GoBackButton } from "mtxuilib/mt/GoBackButton";
 import { ScreenPanel } from "mtxuilib/mt/ScreenPanel";
 import { Button } from "mtxuilib/ui/button";
 
-import { providerMap, signIn } from "mtxuilib/lib/auth/auth";
-import { AuthError } from "next-auth";
-import { redirect } from "next/navigation";
-
-const SIGNIN_ERROR_URL = "/auth/login";
+import { providerMap } from "mtxuilib/lib/auth/auth";
+import {
+  signByCredentials,
+  signByProvider,
+} from "mtxuilib/lib/auth/auth_actions";
 
 export const LoginPage = (props: {
   searchParams: { callbackUrl: string | undefined };
 }) => {
   const callbackUrl = props.searchParams.callbackUrl;
+
+  const handleSubmit = async (formData: FormData) => {
+    await signByCredentials(formData, "/", "/");
+  };
+
   return (
     <ScreenPanel open={true}>
-      <form
-        action={async (formData) => {
-          "use server";
-          try {
-            await signIn("credentials", formData);
-          } catch (error) {
-            if (error instanceof AuthError) {
-              return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
-            }
-            throw error;
-          }
-        }}
-      >
+      <form action={handleSubmit}>
         <label htmlFor="email">
           Email
           <input name="email" id="email" />
@@ -45,25 +38,7 @@ export const LoginPage = (props: {
         <form
           key={provider.id}
           action={async () => {
-            "use server";
-            try {
-              await signIn(provider.id, {
-                redirectTo: props.searchParams?.callbackUrl ?? "",
-              });
-            } catch (error) {
-              // Signin can fail for a number of reasons, such as the user
-              // not existing, or the user not having the correct role.
-              // In some cases, you may want to redirect to a custom error
-              if (error instanceof AuthError) {
-                return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
-              }
-
-              // Otherwise if a redirects happens Next.js can handle it
-              // so you can just re-thrown the error and let Next.js handle it.
-              // Docs:
-              // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
-              throw error;
-            }
+            signByProvider(provider.id, provider.id, "/", "/");
           }}
         >
           <button type="submit">
