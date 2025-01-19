0d159c8ab62e64e72f5f4ea314fb03724116ff27
ci <a@a.com>
2025-01-19 03:58:09 +0000

    1

--- packages/mtxuilib/src/lib/lib-old/middleware.ts
+++ packages/mtxuilib/src/lib/lib-old/middleware.ts
@@ -10,34 +10,34 @@ import { NextResponse } from "next/server";
 
 // 临时配置项
 const MtxMiddleconfig = {
-	enableSp: false,
+  enableSp: false,
 };
 
 export const runtime = "experimental-edge";
 export const config = {
-	matcher: ["/:path*"],
-	unstable_allowDynamic: [
-		"**/node_modules/.pnpm/**/function-bind/**",
-		"**/node_modules/.pnpm/**",
-		"/node_modules/function-bind/**",
-		"/node_modules/.pnpm/function-bind/**",
-	],
+  matcher: ["/:path*"],
+  unstable_allowDynamic: [
+    "**/node_modules/.pnpm/**/function-bind/**",
+    "**/node_modules/.pnpm/**",
+    "/node_modules/function-bind/**",
+    "/node_modules/.pnpm/function-bind/**",
+  ],
 };
 
 export async function middleware(request: Request) {
-	console.log("middleware", request.url);
-	//TODO: 从后端获取配置数据，
-	//           如果配置指定使用原始后端反代，则使用原先的spHandler的方式。
-	//           如果配置执行使用 page.tsx 的方式反代， 则 使用 react html parse 的方式解释并渲染页面。
-	//
-	if (!MtxMiddleconfig.enableSp) {
-		return NextResponse.next();
-	}
-	{
-		const uri = new URL(request.url);
-		const pathName = uri.pathname;
-		if (pathName.startsWith("/api")) {
-			return NextResponse.next();
-		}
-	}
+  console.log("middleware", request.url);
+  //TODO: 从后端获取配置数据，
+  //           如果配置指定使用原始后端反代，则使用原先的spHandler的方式。
+  //           如果配置执行使用 page.tsx 的方式反代， 则 使用 react html parse 的方式解释并渲染页面。
+  //
+  if (!MtxMiddleconfig.enableSp) {
+    return NextResponse.next();
+  }
+  {
+    const uri = new URL(request.url);
+    const pathName = uri.pathname;
+    if (pathName.startsWith("/api")) {
+      return NextResponse.next();
+    }
+  }
 }
