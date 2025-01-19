31c23feb94e6a97dba2e8212038942366b23c580
ci <a@a.com>
2025-01-19 02:46:42 +0000

    1

--- packages/mtxuilib/src/lib/utils.ts
+++ packages/mtxuilib/src/lib/utils.ts
@@ -585,3 +585,34 @@ export function generateUUID(): string {
     return v.toString(16);
   });
 }
+
+export function stripIndents(value: string): string;
+export function stripIndents(
+  strings: TemplateStringsArray,
+  ...values: any[]
+): string;
+
+export function stripIndents(
+  arg0: string | TemplateStringsArray,
+  ...values: any[]
+) {
+  if (typeof arg0 !== "string") {
+    const processedString = arg0.reduce((acc, curr, i) => {
+      const newAcc = acc + curr + (values[i] ?? "");
+      return newAcc;
+    }, "");
+
+    return _stripIndents(processedString);
+  }
+
+  return _stripIndents(arg0);
+}
+
+function _stripIndents(value: string) {
+  return value
+    .split("\n")
+    .map((line) => line.trim())
+    .join("\n")
+    .trimStart()
+    .replace(/[\r\n]$/, "");
+}
