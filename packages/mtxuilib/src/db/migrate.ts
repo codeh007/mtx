4ccb4a47848953922b72d8c031efc16314d4db0e
ci <a@a.com>
2025-01-19 04:34:12 +0000

    77

--- packages/mtxuilib/src/db/migrate.ts
+++ packages/mtxuilib/src/db/migrate.ts
@@ -1,11 +1,11 @@
-import { config } from "dotenv";
+// import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-config({
-  path: "../../dev.env",
-});
+// config({
+//   path: "../../dev.env",
+// });
 
 const runMigrate = async () => {
   if (!process.env.POSTGRES_URL) {
