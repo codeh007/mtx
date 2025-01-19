7c096cb046063eeaed965b0a2e97d4e130986c6e
ci <a@a.com>
2025-01-19 06:52:49 +0000

    1

--- packages/mtxuilib/src/lib/auth/auth.ts
+++ packages/mtxuilib/src/lib/auth/auth.ts
@@ -1,5 +1,30 @@
-import NextAuth from "next-auth";
+import NextAuth, { CredentialsSignin } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
+const superUser = {
+  username: "admin",
+  password: "admin",
+};
+class InvalidLoginError extends CredentialsSignin {
+  code = "Invalid identifier or password";
+}
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        console.log("auth credentials", credentials);
+        if (
+          credentials.username !== superUser.username ||
+          credentials.password !== superUser.password
+        ) {
+          throw new InvalidLoginError();
+        }
+        return superUser;
+      },
+    }),
+  ],
 });
