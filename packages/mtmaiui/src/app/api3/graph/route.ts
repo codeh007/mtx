f116cc1506d51137f4b3698b2f5f4effb53a241c
ci <a@a.com>
2025-01-19 01:29:57 +0000

    1

--- packages/mtmaiui/src/app/api3/graph/route.ts
+++ packages/mtmaiui/src/app/api3/graph/route.ts
@@ -5,13 +5,20 @@ export const runtime = "edge";
 const exampleUserInput = "What's the weather like today?";
 
 const handler = async (r: Request) => {
-  const input = {
-    messages: [{ role: "user", content: exampleUserInput }],
-  };
-  const builder = createAssistantGraph();
-  // const runnable = builder.getRunnable();
-  // const a = runnable.invoke(input);
-  return new Response(JSON.stringify(builder.nodes));
+  try {
+    const input = {
+      messages: [{ role: "user", content: exampleUserInput }],
+    };
+    const builder = createAssistantGraph();
+    // const runnable = builder.getRunnable();
+    // const a = runnable.invoke(input);
+
+    const graph = builder.compile();
+    const result = await graph.invoke(input);
+    return new Response(JSON.stringify(result));
+  } catch (e) {
+    return new Response(JSON.stringify(e));
+  }
 };
 
 export const GET = handler;
