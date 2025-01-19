7f2e9238819be3776d412f06ed062cfc1050b548
ci <a@a.com>
2025-01-19 06:41:04 +0000

    1

--- packages/mtmaiui/src/app/api3/chat/route.ts
+++ packages/mtmaiui/src/app/api3/chat/route.ts
@@ -1,11 +1,26 @@
+import { createAssistantGraph } from "mtxuilib/agents/assisant";
+
 export const runtime = "edge";
-const handler = async (req: Request) => {
-  //   const { messages } = await req.json();
-  //   const response = await chatbot.invoke(messages);
-  //   return NextResponse.json(response);
-  return new Response("Hello chat api");
+
+const exampleUserInput = "What's the weather like today?";
+
+const handler = async (r: Request) => {
+  try {
+    const input = {
+      messages: [{ role: "user", content: exampleUserInput }],
+    };
+    const builder = createAssistantGraph();
+    // const runnable = builder.getRunnable();
+    // const a = runnable.invoke(input);
+
+    const graph = builder.compile();
+    const result = await graph.invoke(input);
+    return new Response(JSON.stringify(result));
+  } catch (e) {
+    console.log("运行graph 出错", e);
+    return new Response(JSON.stringify(e));
+  }
 };
 
-// export { GET, POST } from "next-auth/react";
 export const GET = handler;
 export const POST = handler;
