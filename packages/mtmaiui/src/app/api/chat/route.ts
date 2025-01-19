351293024d2a4584a1d4e2c1e4e75f23fddc4e64
ci <a@a.com>
2025-01-19 13:15:50 +0000

    1

--- packages/mtmaiui/src/app/api/chat/route.ts
+++ packages/mtmaiui/src/app/api/chat/route.ts
@@ -1,27 +1,18 @@
 import { newGraphSseResponse } from "mtxuilib/agentutils/graph_utils";
-import type { GraphInput } from "../../../stores/GraphContext";
 export const runtime = "edge";
 
-const exampleUserInput = "What's the weather like today?";
+// const exampleUserInput = "What's the weather like today?";
 
 const handler = async (r: Request) => {
   try {
-    const input = {
-      messages: [{ role: "user", content: exampleUserInput }],
-    };
+    // const input = {
+    //   messages: [{ role: "user", content: exampleUserInput }],
+    // };
 
     //TODO: 增加 zod schema 验证输入格式
-    const graphInput = (await r.json()) as GraphInput;
-    console.log("graphInput", graphInput);
-
-    // const builder = createAssistantGraph();
-    // // const runnable = builder.getRunnable();
-    // // const a = runnable.invoke(input);
-
-    // const graph = builder.compile();
-    // const result = await graph.invoke(input);
-    // return new Response(JSON.stringify(result));
-    return newGraphSseResponse("test", graphInput, {});
+    // const graphInput =  as GraphInput;
+    // console.log("graphInput", graphInput);
+    return newGraphSseResponse("test", await r.json(), {});
   } catch (e) {
     console.log("run langgraph error", e);
     return new Response(JSON.stringify(e));
