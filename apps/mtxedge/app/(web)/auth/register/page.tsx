85bce68c7aac140d657872046e1ba2c968464dc1
ci <a@a.com>
2025-01-19 10:09:59 +0000

    1

--- apps/mtxedge/app/(web)/auth/register/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-export const runtime = "nodejs";
-
-// import { UserRegisterScreen } from "mtmaiui/components/auth/UserRegister";
-export default function Page(props: { params }) {
-  return <div>{/* <UserRegisterScreen /> */}</div>;
-}
