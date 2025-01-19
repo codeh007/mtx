195b6e0d21ac9b6f54c8992c376f0d3d3e2894ff
ci <a@a.com>
2025-01-19 11:38:59 +0000

    1

--- packages/mtmaiui/src/routes/chat/route.lazy.tsx
+++ packages/mtmaiui/src/routes/chat/route.lazy.tsx
@@ -3,6 +3,7 @@ import { MtSuspenseBoundary } from "mtxuilib/components/MtSuspenseBoundary";
 import { useTenant } from "../../hooks/useAuth";
 import { Canvas } from "../../opencanvasv2/canvas/canvas";
 import { GraphV2Provider } from "../../stores/GraphContextV2";
+import { useMtmaiV2 } from "../../stores/StoreProvider";
 
 export const Route = createLazyFileRoute("/chat")({
   component: RouteComponent,
@@ -10,16 +11,16 @@ export const Route = createLazyFileRoute("/chat")({
 
 function RouteComponent() {
   const tenant = useTenant();
-
+  const selfBackendend = useMtmaiV2((x) => x.selfBackendUrl);
   if (!tenant) {
+    null;
+  }
+  if (!selfBackendend) {
     null;
   }
   return (
     <div className="w-full h-full bg-blue-200 p-2">
-      <GraphV2Provider
-        agentEndpointBase="https://colab-gomtm.yuepa8.com"
-        tenant={tenant}
-      >
+      <GraphV2Provider agentEndpointBase={selfBackendend!} tenant={tenant!}>
         <MtSuspenseBoundary>
           <Canvas />
         </MtSuspenseBoundary>
