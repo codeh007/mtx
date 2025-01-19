0d5c9040e8900f323f645667120995041143b285
ci <a@a.com>
2025-01-19 01:18:36 +0000

    1

--- packages/mtmaiui/src/app/api3/graph/route.ts
+++ packages/mtmaiui/src/app/api3/graph/route.ts
@@ -1,14 +1,13 @@
 import { createAssistantGraph } from "mtxuilib/agents/assisant";
-// import { graphSseStream } from "mtxuilib/graphs/graphutils/graphutils.ts";
 
 export const runtime = "edge";
+
+const exampleUserInput = "What's the weather like today?";
+
 const handler = async (r: Request) => {
   const input = {
-    messages: [{ role: "user", content: "What's the weather like today?" }],
+    messages: [{ role: "user", content: exampleUserInput }],
   };
-  // const a = graphSseStream("assisant", input, {});
-  // const response = newStreamResponse(a);
-  // return response;
   const builder = createAssistantGraph();
   // const runnable = builder.getRunnable();
   // const a = runnable.invoke(input);
