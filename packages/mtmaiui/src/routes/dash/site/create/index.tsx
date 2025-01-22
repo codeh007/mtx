5183a1cdd5874d25e7cfeac52f0d2da9de26eb77
a <a@a.com>
2025-01-22 12:38:49 +0000

    1

--- packages/mtmaiui/src/routes/dash/site/create/index.tsx
+++ packages/mtmaiui/src/routes/dash/site/create/index.tsx
@@ -1,10 +1,79 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { SiteCreateView } from '../../../../components/site/SiteCreate'
+import { useMutation } from "@tanstack/react-query";
+import { type Site, siteCreateMutation } from "mtmaiapi";
 
-export const Route = createFileRoute('/dash/site/create/')({
+import { createFileRoute } from "@tanstack/react-router";
+import { EditFormToolbar } from "mtxuilib/mt/form/EditFormToolbar";
+import { ZForm, useZodForm } from "mtxuilib/mt/form/ZodForm";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "mtxuilib/ui/form";
+import { Input } from "mtxuilib/ui/input";
+import { useTenant } from "../../../../hooks/useAuth";
+
+export const Route = createFileRoute("/dash/site/create/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <SiteCreateView />
+  return <SiteCreateView />;
 }
+
+interface SiteCreateViewProps {
+  onCancel?: () => void;
+  onSuccess?: (data: Site) => void;
+}
+export const SiteCreateView = (props: SiteCreateViewProps) => {
+  const { onCancel, onSuccess } = props;
+  const tenant = useTenant();
+  const form = useZodForm({
+    defaultValues: {},
+  });
+
+  const createSiteMutation = useMutation({
+    ...siteCreateMutation(),
+    onSuccess: (data) => {
+      onSuccess?.(data);
+    },
+  });
+  const handleSubmit = async (data) => {
+    createSiteMutation.mutate({
+      body: data,
+      path: {
+        tenant: tenant!.metadata.id,
+      },
+    });
+  };
+  return (
+    <ZForm
+      form={form}
+      handleSubmit={handleSubmit}
+      className="flex flex-col space-y-2 px-2"
+    >
+      <FormField
+        control={form.control}
+        name="title"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>title</FormLabel>
+            <FormControl>
+              <Input placeholder="title" {...field} />
+            </FormControl>
+            {/* <FormDescription></FormDescription> */}
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      <FormField
+        control={form.control}
+        name="host"
+        render={({ field }) => <Input placeholder="host" {...field} />}
+      />
+      <EditFormToolbar form={form} onCancel={onCancel} />
+    </ZForm>
+  );
+};
