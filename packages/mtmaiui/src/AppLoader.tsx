72c9f08ad094cb4bb4d136fb3f28964a42d88224
a <ci@gmail.com>
2025-01-23 08:33:33 +0000

    1

--- packages/mtmaiui/src/AppLoader.tsx
+++ packages/mtmaiui/src/AppLoader.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-
-
+import { onMount } from "./onMount";
 
 if (typeof window !== "undefined") {
-    console.log("App.tsx");
-    const rootElement = document.getElementById("gomtm-runtime-container");
-    if (rootElement) {
-      //TODO: 需要加载独立的 应用, 还需要正确处理 相关的 Provider
-      createRoot(rootElement).render(<App />);
-    }
+  window.addEventListener("load", () => {
+    onMount();
+  });
+  const rootElement = document.getElementById("gomtm-runtime-container");
+  if (rootElement) {
+    //TODO: 需要加载独立的 应用, 还需要正确处理 相关的 Provider
+    createRoot(rootElement).render(<App />);
   }
-  
+}
--- packages/mtmaiui/src/routes/~__root.tsx
+++ packages/mtmaiui/src/routes/~__root.tsx
@@ -2,11 +2,7 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { UserFAB } from "../components/UserFAB";
 import { NotFound } from "../components/notFound";
-import { onMount } from "./onMount";
 
-if (typeof window !== "undefined") {
-  onMount();
-}
 export const Route = createRootRoute({
   component: RootComponent,
   notFoundComponent: NotFound,
