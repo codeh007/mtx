9be1a96f0c9fd5714953079f854f3accae42483e
a <a@a.com>
2025-01-23 06:14:41 +0000

    1

--- packages/mtmaiui/src/entry-cfworker.ts
+++ packages/mtmaiui/src/entry-cfworker.ts
@@ -1,10 +1,10 @@
-export { WfHelloAssisant } from "mtxuilib/workflows/hello-assisant.ts";
-export { WfHelloToolUse } from "mtxuilib/workflows/hello-tooluse.ts";
+export { WfHelloAssisant } from "mtxuilib/workflows/hello-assisant";
+export { WfHelloToolUse } from "mtxuilib/workflows/hello-tooluse";
 export { MyWorkflow } from "mtxuilib/workflows/hello-workflow";
 export { WorkflowDemo } from "mtxuilib/workflows/WorkflowDemo";
 import { handleWsRequest } from "mtxuilib/routes/ws/wsApp";
 
-import { mainApp } from "mtxuilib/routes/edgeApi.ts";
+import { mainApp } from "mtxuilib/routes/edgeApi";
 
 export default {
   async fetch(request: Request, env, ctx) {
