2a9be0e9a34b0c6b59206f4be819f396cd989c5a
a <a@a.com>
2025-01-25 13:33:51 +0000

    1

--- apps/mtmaiui/src/routes/~endpoint/~index.tsx
+++ apps/mtmaiui/src/routes/~endpoint/~index.tsx
@@ -14,11 +14,13 @@ function RouteComponent() {
   );
 }
 
+const getApiGatewayUrl = () => {
+  return "https://mtmaiui.yuepa8.com/api/v1/agent/ag";
+};
 const TestApiGateway = () => {
   const [responseText, setResponseText] = useState("");
-  const endpointUrl = "https://mtmaiui.yuepa8.com/api/v1/agent/ag";
   const handleClick = async () => {
-    const response = await fetch(endpointUrl);
+    const response = await fetch(getApiGatewayUrl());
     const data = await response.text();
     setResponseText(data);
   };
