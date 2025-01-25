e1e8801a1665d7b1570f2aab5c577fa3baa35cca
a <a@a.com>
2025-01-25 13:17:32 +0000

    1

--- apps/mtmaiui/src/routes/~endpoint/~index.tsx
+++ apps/mtmaiui/src/routes/~endpoint/~index.tsx
@@ -1,9 +1,28 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "mtxuilib/ui/button.jsx";
 
 export const Route = createFileRoute("/endpoint/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/endpoint/"!</div>;
+  return (
+    <div>
+      <HelloApiCall />
+    </div>
+  );
 }
+
+const HelloApiCall = () => {
+  const endpointUrl = "https://mtmaiui.yuepa8.com/api/v1/agent/ag";
+  const handleClick = async () => {
+    const response = await fetch(endpointUrl);
+    const data = await response.text();
+    console.log(data);
+  };
+  return (
+    <div>
+      <Button onClick={handleClick}>HelloApiCall</Button>
+    </div>
+  );
+};
