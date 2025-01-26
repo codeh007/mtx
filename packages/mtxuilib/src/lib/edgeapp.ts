1c445254d3255c5b18a9005e8ffc64d3b0cb45b2
a <a@a.com>
2025-01-26 11:17:16 +0000

    1111

--- packages/mtxuilib/src/lib/edgeapp.ts
+++ packages/mtxuilib/src/lib/edgeapp.ts
@@ -8,6 +8,13 @@ import {
 import type { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 import { isCI, isInBuild } from "./s-utils";
 
+const gomtmBackendToken: string | undefined = undefined;
+let cachedEndpointList: EndpointList | undefined = undefined;
+let cachedFrontendConfig: FrontendConfig | undefined = undefined;
+let isInited = false;
+let headers: () => Promise<Headers> | Headers;
+let cookies: () => Promise<ReadonlyRequestCookies>;
+
 export function getBackendUrl(prefix = "") {
   if (process.env.MTM_BASE_URL) {
     return `${process.env.MTM_BASE_URL}${prefix}`;
@@ -37,176 +44,142 @@ export async function initMtiaiClientV2() {
   });
 }
 
-export class EdgeApp {
-  private isInited = false;
-  public hostName = "";
-  public token?: string = undefined;
-  public frontendConfig?: FrontendConfig = undefined;
-  //业务后端列表
-  public endpointList?: EndpointList = undefined;
-
-  private cookies?: () => Promise<ReadonlyRequestCookies>;
-  private headers?: () => Promise<Headers> | Headers;
-  /**
-   * 应用的初始化
-   * 在 nextjs 中的 layout page route 页面中,需要调用此方法
-   * @param opts
-   * @returns
-   */
-  async init(opts: {
-    cookies?: () => Promise<ReadonlyRequestCookies>;
-    headers?: () => Promise<Headers> | Headers;
-  }) {
-    if (this.isInited) {
-      console.log("edgeApp.init already inited");
-      return;
-    }
-    this.token = process.env?.MTM_ADMIN_TOKEN;
-    if (!this.token && !isCI() && !isInBuild()) {
-      throw new Error("MTM_ADMIN_TOKEN is not set");
-    }
-
-    if (!this.token) {
-      console.warn("⚠️ MTM_ADMIN_TOKEN is not set");
-    }
-    if (typeof window !== "undefined") {
-      return;
-    }
-    this.headers = opts.headers;
-    this.cookies = opts.cookies;
-    // initMtiaiClient({
-    //   baseUrl: (await getBackendUrl()) || "http://localhost:3000",
-    // });
-    await initMtiaiClientV2();
-    this.isInited = true;
+export async function getEndpointList() {
+  if (cachedEndpointList) {
+    return cachedEndpointList;
   }
-
-  async getAccessToken() {
-    if (isInBuild()) {
-      console.warn("在build 阶段,不加载AccessToken");
-      return "";
-    }
-    const frontendConfig = await this.getFrontendConfig();
-    if (!frontendConfig) {
-      throw new Error("get frontendConfig error");
-    }
-    const tokenName = frontendConfig?.cookieAccessToken;
-    if (this.cookies) {
-      return (await this.cookies()).get(tokenName)?.value || "";
-    }
-    return "";
+  console.log(`getEndpointList, backendUrl: ${await getBackendUrl()}`);
+  try {
+    cachedEndpointList = (await endpointList({})).data;
+  } catch (e) {
+    console.error(
+      `getEndpointList error: ${e}, when backendUrl: ${await getBackendUrl()}`,
+    );
   }
+}
 
-  async reset() {
-    this.endpointList = undefined;
-    this.frontendConfig = undefined;
+export async function loadRemoteEnv() {
+  const envUrl = `${await getBackendUrl()}/api/v1/env/default`;
+  const response = await fetch(envUrl, {
+    headers: {
+      ...(gomtmBackendToken && {
+        Authorization: `Bearer ${gomtmBackendToken}`,
+      }),
+    },
+  });
+  if (!response.ok) {
+    throw new Error(
+      `get remote env error: ${response.statusText}, from url: ${envUrl}`,
+    );
   }
+  const envText = await response.text();
+  const env = envText.split("\n").map((line) => line.trim());
 
-  // 获取当前服务器的url(通常是nextjs 运行的服务器地址)
-  // async getBackendUrl(prefix = "") {
-  //   if (process.env.MTM_BASE_URL) {
-  //     return `${process.env.MTM_BASE_URL}${prefix}`;
-  //   }
-  //   if (process.env.MTMAI_BACKEND) {
-  //     return `${process.env.MTMAI_BACKEND}${prefix}`;
-  //   }
-  //   if (process.env.VERCEL_URL) {
-  //     return `https://${process.env.VERCEL_URL}${prefix}`;
-  //   }
-  //   const host = this.hostName || "localhost";
-  //   const port = Number(process.env.PORT) || 3000;
-
-  //   const localHost =
-  //     host?.includes("localhost") ||
-  //     host?.includes("ts.net") ||
-  //     host?.startsWith("100.");
-  //   if (localHost) {
-  //     return `http://${host}:${port}${prefix}`;
-  //   }
-  //   return `https://${host}:${port}${prefix}`;
-  // }
-
-  // 获取前端配置数据
-  async getFrontendConfig() {
-    if (isInBuild()) {
-      console.warn("在build 阶段,不加载FrontendConfig远程前端配置");
-      return {} as FrontendConfig;
+  const skipsComment = ["#", "//"];
+  //防止覆盖重要本地变量
+  const skipsLocals = [
+    "NEXT_BUILD_OUTPUT",
+    "PORT",
+    "MTMAI_BACKEND",
+    "MTM_TOKEN",
+  ];
+  for (const line of env) {
+    const v = line.split("=");
+    if (v.length !== 2) {
+      continue;
     }
-    if (!this.frontendConfig) {
-      try {
-        this.frontendConfig = (await frontendGetConfig({})).data;
-      } catch (e) {
-        console.error(`getFrontendConfig error: ${e}`);
-        this.frontendConfig = undefined;
-      }
+    const envName = v[0];
+    if (skipsComment.includes(envName)) {
+      continue;
     }
-    return this.frontendConfig;
-  }
-
-  // 获取业务后端列表
-  async getEndpointList() {
-    if (!this.endpointList) {
-      console.log(`getEndpointList, backendUrl: ${await getBackendUrl()}`);
-      try {
-        this.endpointList = (await endpointList({})).data;
-      } catch (e) {
-        console.error(
-          `getEndpointList error: ${e}, when backendUrl: ${await getBackendUrl()}`,
-        );
-      }
+    if (skipsLocals.includes(envName)) {
+      continue;
     }
-    return this.endpointList;
-  }
-
-  async loadRemoteEnv() {
-    const envUrl = `${await getBackendUrl()}/api/v1/env/default`;
-    const response = await fetch(envUrl, {
-      headers: {
-        Authorization: `Bearer ${this.token}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error(
-        `get remote env error: ${response.statusText}, from url: ${envUrl}`,
-      );
-    }
-    const envText = await response.text();
-    const env = envText.split("\n").map((line) => line.trim());
-
-    const skipsComment = ["#", "//"];
-    //防止覆盖重要本地变量
-    const skipsLocals = [
-      "NEXT_BUILD_OUTPUT",
-      "PORT",
-      "MTMAI_BACKEND",
-      "MTM_TOKEN",
-    ];
-    for (const line of env) {
-      const v = line.split("=");
-      if (v.length !== 2) {
-        continue;
+    let envValue = line.split("=")[1];
+    if (envValue) {
+      envValue = envValue.trim();
+      if (envValue.startsWith('"') && envValue.endsWith('"')) {
+        envValue = envValue.substring(1, envValue.length - 1);
       }
-      const envName = v[0];
-      if (skipsComment.includes(envName)) {
-        continue;
+      if (envValue.startsWith("'") && envValue.endsWith("'")) {
+        envValue = envValue.substring(1, envValue.length - 1);
       }
-      if (skipsLocals.includes(envName)) {
-        continue;
-      }
-      let envValue = line.split("=")[1];
-      if (envValue) {
-        envValue = envValue.trim();
-        if (envValue.startsWith('"') && envValue.endsWith('"')) {
-          envValue = envValue.substring(1, envValue.length - 1);
-        }
-        if (envValue.startsWith("'") && envValue.endsWith("'")) {
-          envValue = envValue.substring(1, envValue.length - 1);
-        }
-        console.log(`⚠️ [remote] ${envName}=${envValue}`);
-        process.env[envName] = envValue;
-      }
+      console.log(`⚠️ [remote] ${envName}=${envValue}`);
+      process.env[envName] = envValue;
     }
   }
 }
 
-export const edgeApp = new EdgeApp();
+// 获取前端配置数据
+export async function getFrontendConfig() {
+  if (isInBuild()) {
+    console.warn("在build 阶段,不加载FrontendConfig远程前端配置");
+    return {} as FrontendConfig;
+  }
+  if (!cachedFrontendConfig) {
+    try {
+      cachedFrontendConfig = (await frontendGetConfig({})).data;
+    } catch (e) {
+      console.error(`getFrontendConfig error: ${e}`);
+      cachedFrontendConfig = undefined;
+    }
+  }
+  return cachedFrontendConfig;
+}
+
+export async function getAccessToken() {
+  if (isInBuild()) {
+    console.warn("在build 阶段,不加载AccessToken");
+    return "";
+  }
+  const frontendConfig = await getFrontendConfig();
+  if (!frontendConfig) {
+    throw new Error("get frontendConfig error");
+  }
+  // const tokenName = frontendConfig?.cookieAccessToken;
+  // if (this.cookies) {
+  //   return (await this.cookies()).get(tokenName)?.value || "";
+  // }
+  return "";
+}
+
+export async function reset() {
+  cachedEndpointList = undefined;
+  cachedFrontendConfig = undefined;
+}
+
+// 获取当前 http 主机名
+export async function getHostName() {
+  if (typeof window !== "undefined") {
+    return window.location.hostname;
+  }
+  if (headers) {
+    return (await headers()).get("host") || "localhost";
+  }
+  return "localhost";
+}
+
+export async function initEdgeApp(opts: {
+  cookies?: () => Promise<ReadonlyRequestCookies>;
+  headers?: () => Promise<Headers> | Headers;
+}) {
+  if (isInited) {
+    console.log("edgeApp.init already inited");
+    return;
+  }
+  const token = process.env?.MTM_ADMIN_TOKEN;
+  if (!token && !isCI() && !isInBuild()) {
+    throw new Error("MTM_ADMIN_TOKEN is not set");
+  }
+
+  if (!token) {
+    console.warn("⚠️ MTM_ADMIN_TOKEN is not set");
+  }
+  if (typeof window !== "undefined") {
+    return;
+  }
+  headers = opts.headers;
+  cookies = opts.cookies;
+  await initMtiaiClientV2();
+  isInited = true;
+}
