545de875ca6ee71b63fea1b2e7c3ea8a7befe6e4
a <a@a.com>
2025-01-26 08:32:21 +0000

    1

--- packages/gomtm-cli/src/app/layout.tsx
+++ packages/gomtm-cli/src/app/layout.tsx
@@ -1,12 +1,4 @@
-// import "@xterm/xterm/css/xterm.css";
-// import "mtxuilib/styles/globals.css";
-// import type { Viewport } from "next";
-// import type { ReactNode } from "react";
-// import "react-toastify/dist/ReactToastify.css";
-
 import type { ReactNode } from "react";
-
-// import "../../styles/globals.css";
 export const runtime = "edge"; //nodejs
 export const dynamic = "force-dynamic";
 
