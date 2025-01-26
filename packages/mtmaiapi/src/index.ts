47615c66e8c912fa5282262ff51d22e2ec3888f5
a <a@a.com>
2025-01-26 10:37:05 +0000

    2

--- packages/mtmaiapi/src/index.ts
+++ packages/mtmaiapi/src/index.ts
@@ -7,6 +7,14 @@ export const cookieAccessToken = "mtm_access_token";
 
 export function initMtiaiClient(backendUrl: string) {
   console.log("initMtiaiClient", backendUrl);
+  if (!backendUrl) {
+    throw new Error("initMtiaiClient backendUrl is not set");
+  }
+  try {
+    const uri = new URL(backendUrl);
+  } catch (e) {
+    console.error("initMtiaiClient backendUrl is not a valid url", e);
+  }
   client?.setConfig({
     baseUrl: backendUrl,
     // fetch:  async (req) => {
