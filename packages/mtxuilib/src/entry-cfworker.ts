ec79638ae6c01eea1fb086ed3e3690c67b1a2cad
repeat001003 <repeat001003@protonmail.com>
2025-01-28 07:20:12 +0000

    1

--- packages/mtxuilib/src/entry-cfworker.ts
+++ packages/mtxuilib/src/entry-cfworker.ts
@@ -6,17 +6,23 @@ export { MyWorkflow } from "./workflows/hello-workflow";
 
 import type { EndpointList, Env } from "mtmaiapi/gomtmapi/types.gen";
 import { getEndpointList, initEdgeApp } from "./lib/sslib";
+import { mainApp } from "./routes/edgeApi";
 import { handleWsRequest } from "./routes/ws/wsApp";
 export default {
   async fetch(request: Request, env, ctx) {
     //设置环境变量,达到兼容 nodejs 的目的
     for (const key in env) {
       if (typeof env[key] === "string") {
-        // console.log(`set env:${key}=${env[key]}`);
         process.env[key] = env[key];
       }
     }
     try {
+      // console.log(`env:${JSON.stringify(env)}`);
+      await initEdgeApp({});
+      const responseFromApp = await mainApp.fetch(request, env, ctx);
+      if (responseFromApp.status === 200) {
+        return responseFromApp;
+      }
       const uri = new URL(request.url);
       if (uri.pathname.startsWith("/api/ws")) {
         return handleWsRequest(request, env, ctx);
@@ -75,30 +81,51 @@ export default {
  * 业务api 反向代理
  */
 const proxyHandler = async (r: Request, env?: Env, ctx?: ExecutionContext) => {
-  console.log(`env:${JSON.stringify(env)}`);
-  await initEdgeApp({});
-
   let endpointList: EndpointList | undefined = undefined;
   endpointList = await getEndpointList();
 
   if (!endpointList?.rows?.length) {
-    throw new Error("endpointList is empty");
+    // @ts-ignore
+    process.env.DEFAULT_HFSPACE = env.DEFAULT_HFSPACE;
+    console.log(`使用环境变量的默认值:${process.env.DEFAULT_HFSPACE}`);
+
+    const uri = new URL(process.env.DEFAULT_HFSPACE);
+    // 创建一个新的 URL，只保留协议、主机名和路径部分
+    const cleanUrl = `${uri.protocol}//${uri.host}${uri.pathname}${uri.search}`;
+
+    endpointList = {
+      rows: [],
+    };
+    endpointList.rows.push({
+      url: cleanUrl, // 使用清理后的 URL
+      token: uri.password,
+      metadata: {
+        id: "default-hfspace",
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      },
+      name: "default-hfspace",
+      type: "hfspace",
+    });
+    console.log(`endpointList:${JSON.stringify(endpointList)}`);
   }
   //开发阶段使用第一条配置作为远程服务器的配置
-  const targetEndpoint = endpointList[0];
+  const targetEndpoint = endpointList.rows[0];
   const remoteUrl = targetEndpoint.url;
   const token = targetEndpoint.token;
 
   const url = new URL(r.url);
-  const targetUrl = `${remoteUrl}${url.pathname}${url.search}`;
+  const targetUrl = new URL(`${remoteUrl}${url.pathname}${url.search}`);
+  targetUrl.password = undefined;
+  targetUrl.username = undefined;
   const requestHeaders = new Headers(r.headers);
   requestHeaders.set("Authorization", `Bearer ${token}`);
 
   console.log(
-    `🚀 [endpoint api proxy] ${r.method} ${targetUrl} len:${r.body?.length}`,
+    `🚀 [endpoint api proxy(v2)] ${r.method} ${targetUrl.toString()} len:${r.body?.length}`,
   );
   // Forward the request with same method, headers and body
-  const response = await fetch(targetUrl, {
+  const response = await fetch(targetUrl.toString(), {
     method: r.method,
     headers: requestHeaders,
     body: ["GET", "HEAD"].includes(r.method) ? undefined : r.body,
