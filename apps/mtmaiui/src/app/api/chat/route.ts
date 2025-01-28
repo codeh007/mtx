fcb26a16f7e8aac2d07243ab480b7cc68639679a
repeat001003 <repeat001003@protonmail.com>
2025-01-28 15:44:21 +0000

    1

--- apps/mtmaiui/src/app/api/chat/route.ts
+++ apps/mtmaiui/src/app/api/chat/route.ts
@@ -1,27 +1,43 @@
-import { newGraphSseResponse } from "mtxuilib/agentutils/graph_utils";
-import { cookies } from "next/headers";
+import { openai } from "@ai-sdk/openai";
+// import { newGraphSseResponse } from "mtxuilib/agentutils/graph_utils";
+// import { cookies } from "next/headers";
+// import type { ChatCompletionCreateParams, ChatCompletionMessageParam } from "openai/resources/chat/completions";
+
+import { streamText } from "ai";
+
 export const runtime = "edge";
 
+export const maxDuration = 30;
 const handler = async (r: Request) => {
-  try {
-    const accessToken =
-      (await cookies()).get("access_token")?.value ||
-      r.headers.get("Authorization");
-    if (!accessToken) {
-      throw new Error("accessToken is required");
-    }
-    return newGraphSseResponse("storm", await r.json(), {
-      configurable: {
-        ctx: {
-          accessToken: accessToken,
-        },
-        backendUrl: process.env.MTMAI_BACKEND,
-      },
-    });
-  } catch (e) {
-    console.log("run langgraph error", e);
-    return new Response(JSON.stringify(e));
-  }
+  const { messages } = (await r.json()) as any;
+
+  const result = streamText({
+    model: openai("gpt-4o"),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+
+  // 备忘代码,调用 langgraph
+  // try {
+  //   const accessToken =
+  //     (await cookies()).get("access_token")?.value ||
+  //     r.headers.get("Authorization");
+  //   if (!accessToken) {
+  //     throw new Error("accessToken is required");
+  //   }
+  //   return newGraphSseResponse("storm", await r.json(), {
+  //     configurable: {
+  //       ctx: {
+  //         accessToken: accessToken,
+  //       },
+  //       backendUrl: process.env.MTMAI_BACKEND,
+  //     },
+  //   });
+  // } catch (e) {
+  //   console.log("run langgraph error", e);
+  //   return new Response(JSON.stringify(e));
+  // }
 };
 
 export const GET = handler;
