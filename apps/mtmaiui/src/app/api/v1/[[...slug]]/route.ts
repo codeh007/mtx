a8820a23df233e09ce3a1feadbafb9c12225d147
nmiitmo <nmiitmo@proton.me>
2025-02-02 10:00:27 +0000

    111111

--- apps/mtmaiui/src/app/api/v1/[[...slug]]/route.ts
+++ apps/mtmaiui/src/app/api/v1/[[...slug]]/route.ts
@@ -1,5 +1,5 @@
 import { getBackendUrl, initGomtmApp } from "mtmaiapi/gomtmapp";
-import { copyIncomeHeaders } from "mtxuilib/http/rproxy";
+import { newRProxy } from "mtxuilib/http/rproxy";
 
 export const runtime = "edge";
 
@@ -7,18 +7,21 @@ export const runtime = "edge";
  * 业务api 反向代理
  */
 const handler = async (r: Request) => {
-  await initGomtmApp({ r: r });
-  const url = new URL(r.url);
-  const backendUrl = getBackendUrl();
-  const targetUrl = `${backendUrl}${url.pathname}${url.search}`;
-  const requestHeaders = copyIncomeHeaders(r);
-  const response = await fetch(targetUrl, {
-    method: r.method,
-    headers: requestHeaders,
-    body: ["GET", "HEAD"].includes(r.method) ? undefined : r.body,
-  });
-  console.log(`🚀 [api proxy] ${r.method}(${response.status}) ${targetUrl}`);
-  return response;
+	await initGomtmApp({ r: r });
+	const rProxy = newRProxy({
+		baseUrl: getBackendUrl(),
+		rewrites: [
+			{
+				from: "/api/v1/chat",
+				to: "https://service1.example.com",
+			},
+			{
+				from: /^\/api\/service2/,
+				to: "https://service2.example.com",
+			},
+		],
+	});
+	return rProxy(r);
 };
 
 export const GET = handler;
