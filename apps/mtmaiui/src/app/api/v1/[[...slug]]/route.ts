38c94a70718f8bf1cb6c9b83b69647713c825117
nmiitmo <nmiitmo@proton.me>
2025-02-03 02:01:53 +0000

    5555

--- apps/mtmaiui/src/app/api/v1/[[...slug]]/route.ts
+++ apps/mtmaiui/src/app/api/v1/[[...slug]]/route.ts
@@ -12,17 +12,12 @@ const handler = async (r: Request) => {
     baseUrl: getBackendUrl(),
     rewrites: [
       {
-        //https://colab-3600.yuepa8.com/api/v1/tenants/707d0855-80ab-4e1f-a156-f1c4546cbf52/chat
         from: "/api/v1/tenants/:tenant/chat",
         to: "http://localhost:7860/api/v1/tenants/:tenant/chat",
       },
       {
         from: "/api/v1/chat",
         to: "http://localhost:7860",
-      },
-      {
-        from: /^\/api\/service2abde\/notexist/,
-        to: "https://service2.example.com",
       },
     ],
   });
