fe92f866b67b1588e063dd4630799c5950360b27
a <a.com>
2025-02-07 09:14:57 +0800

    1

--- packages/mtmaiapi/openapi-ts.config.ts
+++ packages/mtmaiapi/openapi-ts.config.ts
@@ -1,32 +1,32 @@
 import {
-	type UserConfig,
-	defaultPlugins,
-	defineConfig,
+  type UserConfig,
+  defaultPlugins,
+  defineConfig,
 } from "@hey-api/openapi-ts";
 
 //新版，使用golang openapi 后端
 export default defineConfig({
-	client: "@hey-api/client-fetch",
-	input: "../../../gomtm/api-contracts/openapi/openapi.yaml",
-	output: {
-		format: "prettier",
-		lint: "eslint",
-		path: "./src/gomtmapi",
-	},
-	// enums: "typescript",
-	experimentalParser: true,
-	plugins: [
-		...defaultPlugins,
-		// '@hey-api/schemas',
-		"@tanstack/react-query",
-		// "zod",
-		// {
-		//   name: "@hey-api/sdk",
-		//   validator: true,
-		// },
-	],
-	// types: {
-	//   enums: "javascript",
-	//   // name: "@hey-api/typescript",
-	// },
+  // client: "@hey-api/client-fetch",
+  input: "../../../gomtm/api-contracts/openapi/openapi.yaml",
+  output: {
+    format: "prettier",
+    lint: "eslint",
+    path: "./src/gomtmapi",
+  },
+  // enums: "typescript",
+  experimentalParser: true,
+  plugins: [
+    ...defaultPlugins,
+    // '@hey-api/schemas',
+    "@tanstack/react-query",
+    // "zod",
+    // {
+    //   name: "@hey-api/sdk",
+    //   validator: true,
+    // },
+  ],
+  // types: {
+  //   enums: "javascript",
+  //   // name: "@hey-api/typescript",
+  // },
 }) as UserConfig;
