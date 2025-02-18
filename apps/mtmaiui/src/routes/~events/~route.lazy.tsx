03f8535b41b53f167508b06aa7dea2b6b3721358
cnapp202 <cnapp202@proton.me>
2025-02-18 07:01:12 +0000

    1

--- apps/mtmaiui/src/routes/~events/~route.lazy.tsx
+++ apps/mtmaiui/src/routes/~events/~route.lazy.tsx
@@ -1,21 +1,21 @@
-'use client'
-import { Outlet, createLazyFileRoute } from '@tanstack/react-router'
+"use client";
+import { Outlet, createLazyFileRoute } from "@tanstack/react-router";
 
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbList,
   BreadcrumbPage,
-} from 'mtxuilib/ui/breadcrumb'
-import { SidebarInset } from 'mtxuilib/ui/sidebar'
-import { Suspense } from 'react'
-import { DashContent } from '../../components/DashContent'
-import { DashHeaders } from '../../components/DashHeaders'
-import { DashSidebar } from '../../components/sidebar/siderbar'
-import { RootAppWrapper } from '../components/RootAppWrapper'
-export const Route = createLazyFileRoute('/events')({
+} from "mtxuilib/ui/breadcrumb";
+import { SidebarInset } from "mtxuilib/ui/sidebar";
+import { Suspense } from "react";
+import { DashContent } from "../../components/DashContent";
+import { DashHeaders } from "../../components/DashHeaders";
+import { DashSidebar } from "../../components/sidebar/siderbar";
+import { RootAppWrapper } from "../components/RootAppWrapper";
+export const Route = createLazyFileRoute("/events")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
@@ -26,7 +26,7 @@ function RouteComponent() {
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem>
-                <BreadcrumbPage>Workflows</BreadcrumbPage>
+                <BreadcrumbPage>Events</BreadcrumbPage>
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
@@ -38,5 +38,5 @@ function RouteComponent() {
         </DashContent>
       </SidebarInset>
     </RootAppWrapper>
-  )
+  );
 }
