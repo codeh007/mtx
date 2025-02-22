e6eef185b3799be8c3d35802406408c012821752
naiixo0 <naiixo0@proton.me>
2025-02-22 01:44:45 +0000

    1

--- apps/mtmaiui/src/app/(web)/connect-query/StreamExample1.tsx
+++ apps/mtmaiui/src/app/(web)/connect-query/StreamExample1.tsx
@@ -1,26 +1,29 @@
 "use client";
-import { createClient } from "@connectrpc/connect";
-import { createConnectTransport } from "@connectrpc/connect-web";
 import { AgService } from "mtmaiapi/mtmclient/mtm/sppb/ag_pb";
 import { Button } from "mtxuilib/ui/button";
+import { useState } from "react";
+import { useGomtmClient } from "../../../stores/TransportProvider";
 
 export const StreamExample1 = () => {
-  const handleClient = async () => {
-    const transport = createConnectTransport({
-      baseUrl: "https://colab-gomtm.yuepa8.com",
-    });
+  const client = useGomtmClient(AgService);
 
-    // Here we make the client itself, combining the service
-    // definition with the transport.
-    const client = createClient(AgService, transport);
+  const [results, setResults] = useState<string[]>([]);
+  const handleClient = async () => {
     const stream = client.greet2({ name: "world" });
     for await (const response of stream) {
       console.log("Received greeting:", response.greeting);
+      setResults((pre) => [...pre, response.greeting]);
     }
   };
   return (
     <div>
       <Button onClick={handleClient}>StreamExample1</Button>
+      <div>
+        {results.map((result, index) => (
+          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+          <div key={index}>{result}</div>
+        ))}
+      </div>
     </div>
   );
 };
--- apps/mtmaiui/src/stores/TransportProvider.tsx
+++ apps/mtmaiui/src/stores/TransportProvider.tsx
@@ -1,9 +1,28 @@
+import type { DescService } from "@bufbuild/protobuf";
+import { type Client, createClient } from "@connectrpc/connect";
 import { TransportProvider } from "@connectrpc/connect-query";
 import { createConnectTransport } from "@connectrpc/connect-web";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useMemo } from "react";
 
-const finalTransport = createConnectTransport({
+export const transport = createConnectTransport({
   baseUrl: "https://colab-gomtm.yuepa8.com",
+  // By default, this transport uses the JSON format.
+  // Set this option to true to use the binary format.
+  useBinaryFormat: false,
+
+  // Interceptors apply to all calls running through this transport.
+  interceptors: [],
+
+  // By default, all requests use POST. Set this option to true to use GET
+  // for side-effect free RPCs.
+  useHttpGet: false,
+
+  // Optional override of the fetch implementation used by the transport.
+  fetch: globalThis.fetch,
+
+  // Options for Protobuf JSON serialization.
+  jsonOptions: {},
 });
 
 const queryClient = new QueryClient();
@@ -12,8 +31,16 @@ export function MtTransportProvider({
   children,
 }: { children: React.ReactNode }) {
   return (
-    <TransportProvider transport={finalTransport}>
+    <TransportProvider transport={transport}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </TransportProvider>
   );
+}
+
+/**
+ * Get a promise client for the given service.
+ */
+export function useGomtmClient<T extends DescService>(service: T): Client<T> {
+  // We memoize the client, so that we only create one instance per service.
+  return useMemo(() => createClient(service, transport), [service]);
 }
