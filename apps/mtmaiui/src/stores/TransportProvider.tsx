6599232d645607f1ea0c80229cd1360459e38725
naiixo0 <naiixo0@proton.me>
2025-02-22 03:25:19 +0000

    1

--- apps/mtmaiui/src/stores/TransportProvider.tsx
+++ apps/mtmaiui/src/stores/TransportProvider.tsx
@@ -5,31 +5,32 @@ import { createConnectTransport } from "@connectrpc/connect-web";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useMemo } from "react";
 
-export const transport = createConnectTransport({
-  baseUrl: "https://colab-gomtm.yuepa8.com",
-  // By default, this transport uses the JSON format.
-  // Set this option to true to use the binary format.
-  useBinaryFormat: false,
+// export const transport = createConnectTransport({
+//   baseUrl: "https://colab-gomtm.yuepa8.com",
+//   // By default, this transport uses the JSON format.
+//   // Set this option to true to use the binary format.
+//   useBinaryFormat: false,
 
-  // Interceptors apply to all calls running through this transport.
-  interceptors: [],
+//   // Interceptors apply to all calls running through this transport.
+//   interceptors: [],
 
-  // By default, all requests use POST. Set this option to true to use GET
-  // for side-effect free RPCs.
-  useHttpGet: false,
+//   // By default, all requests use POST. Set this option to true to use GET
+//   // for side-effect free RPCs.
+//   useHttpGet: false,
 
-  // Optional override of the fetch implementation used by the transport.
-  fetch: globalThis.fetch,
+//   // Optional override of the fetch implementation used by the transport.
+//   fetch: globalThis.fetch,
 
-  // Options for Protobuf JSON serialization.
-  jsonOptions: {},
-});
+//   // Options for Protobuf JSON serialization.
+//   jsonOptions: {},
+// });
 
 const queryClient = new QueryClient();
 
 export function MtTransportProvider({
   children,
 }: { children: React.ReactNode }) {
+  const transport = useGomtmTransport();
   return (
     <TransportProvider transport={transport}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
@@ -37,10 +38,37 @@ export function MtTransportProvider({
   );
 }
 
+export function useGomtmTransport() {
+  const transport = useMemo(
+    () =>
+      createConnectTransport({
+        baseUrl: "http://localhost:8383",
+        // By default, this transport uses the JSON format.
+        // Set this option to true to use the binary format.
+        useBinaryFormat: false,
+
+        // Interceptors apply to all calls running through this transport.
+        interceptors: [],
+
+        // By default, all requests use POST. Set this option to true to use GET
+        // for side-effect free RPCs.
+        useHttpGet: false,
+
+        // Optional override of the fetch implementation used by the transport.
+        fetch: globalThis.fetch,
+
+        // Options for Protobuf JSON serialization.
+        jsonOptions: {},
+      }),
+    [],
+  );
+  return transport;
+}
 /**
  * Get a promise client for the given service.
  */
 export function useGomtmClient<T extends DescService>(service: T): Client<T> {
   // We memoize the client, so that we only create one instance per service.
-  return useMemo(() => createClient(service, transport), [service]);
+  const transport = useGomtmTransport();
+  return useMemo(() => createClient(service, transport), [service, transport]);
 }
