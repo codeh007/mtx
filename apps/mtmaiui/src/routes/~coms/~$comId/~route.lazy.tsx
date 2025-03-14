ab851b7c3a45f19f18181f38cd58654a0ef6cdd7
looobjn88761 c <looobjn88761@proton.me>
2025-03-14 12:10:20 +0000

    1

--- apps/mtmaiui/src/routes/~coms/~$comId/~route.lazy.tsx
+++ apps/mtmaiui/src/routes/~coms/~$comId/~route.lazy.tsx
@@ -2,7 +2,6 @@ import { Outlet, createLazyFileRoute } from "@tanstack/react-router";
 import { AgService } from "mtmaiapi/mtmclient/mtmai/mtmpb/ag_pb";
 import { AgentRpc } from "mtmaiapi/mtmclient/mtmai/mtmpb/agent_worker_pb";
 import { Dispatcher } from "mtmaiapi/mtmclient/mtmai/mtmpb/dispatcher_pb";
-import { EventsService } from "mtmaiapi/mtmclient/mtmai/mtmpb/events_pb";
 import { useTenant } from "../../../hooks/useAuth";
 import { useMtmaiV2 } from "../../../stores/StoreProvider";
 import { useGomtmClient } from "../../../stores/TransportProvider";
@@ -24,14 +23,14 @@ function RouteComponent() {
 
   const mtmAgClient = useGomtmClient(AgService);
   const agrpcClient = useGomtmClient(AgentRpc);
-  const eventClient = useGomtmClient(EventsService);
+  // const eventClient = useGomtmClient(EventsService);
   const dispatcherClient = useGomtmClient(Dispatcher);
   const nav = Route.useNavigate();
   return (
     <>
       <WorkbrenchProvider
         agClient={mtmAgClient}
-        eventClient={eventClient}
+        // eventClient={eventClient}
         dispatcherClient={dispatcherClient}
         runtimeClient={agrpcClient}
         backendUrl={selfBackendend!}
