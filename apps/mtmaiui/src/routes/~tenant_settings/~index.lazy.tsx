f5b44dfb01496cd891bfb87d0337620efda3485c
sj87xapap x <sj87xapap@proton.me>
2025-03-16 04:54:07 +0000

    1

--- apps/mtmaiui/src/routes/~tenant_settings/~index.lazy.tsx
+++ apps/mtmaiui/src/routes/~tenant_settings/~index.lazy.tsx
@@ -1,18 +1,54 @@
+import { useMutation } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { FlowNames, workflowRunCreateMutation } from "mtmaiapi";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "mtxuilib/ui/breadcrumb";
 import { Button } from "mtxuilib/ui/button";
+import { DashHeaders } from "../../components/DashHeaders";
 
 export const Route = createLazyFileRoute("/tenant_settings/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const mutateWorkflowRun = useMutation({
+    ...workflowRunCreateMutation({
+      path: {
+        workflow: FlowNames.TENANT_SETTINGS,
+      },
+    }),
+  });
   const handleResetTenandSetting = () => {
-    console.log("handleResetTenandSetting");
+    console.log("handleResetTenandSetting", FlowNames.TENANT_SETTINGS);
+    mutateWorkflowRun.mutateAsync({
+      path: {
+        workflow: FlowNames.TENANT_SETTINGS,
+      },
+      body: {
+        input: {
+          content: "重置租户配置",
+        },
+      },
+    });
   };
   return (
-    <div>
-      <div>Tenant Settings</div>
-      <Button onClick={handleResetTenandSetting}>重置租户配置</Button>
-    </div>
+    <>
+      <DashHeaders>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbPage>租户设置</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </DashHeaders>
+      <div className="flex flex-col h-full w-full px-2">
+        <Button onClick={handleResetTenandSetting}>重置租户配置</Button>
+      </div>
+    </>
   );
 }
