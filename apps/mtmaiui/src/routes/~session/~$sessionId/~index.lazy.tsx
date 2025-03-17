1f5f7c21211b42c3f53b20ea259f6577662e2896
sj87xapap x <sj87xapap@proton.me>
2025-03-17 12:31:56 +0000

    1

--- apps/mtmaiui/src/routes/~session/~$sessionId/~index.lazy.tsx
+++ apps/mtmaiui/src/routes/~session/~$sessionId/~index.lazy.tsx
@@ -1,34 +1,31 @@
-import { useQuery } from '@tanstack/react-query'
-import { createLazyFileRoute } from '@tanstack/react-router'
-import { agStateGetOptions } from 'mtmaiapi'
-import { DebugValue } from 'mtxuilib/components/devtools/DebugValue'
-import { useTenantId } from '../../../hooks/useAuth'
-import { ChatClient } from '../../~play/~chat/chat/Chat.client'
+import { useQuery } from "@tanstack/react-query";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { agStateGetOptions } from "mtmaiapi";
+import { DebugValue } from "mtxuilib/components/devtools/DebugValue";
+import { useTenantId } from "../../../hooks/useAuth";
+import { ChatClient } from "../../~play/~chat/chat/Chat.client";
 
-export const Route = createLazyFileRoute('/session/$sessionId/')({
+export const Route = createLazyFileRoute("/session/$sessionId/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  const { sessionId } = Route.useParams()
-  const tid = useTenantId()
+  const { sessionId } = Route.useParams();
+  const tid = useTenantId();
   const agStateQuery = useQuery({
     ...agStateGetOptions({
       path: {
         tenant: tid!,
-        // sessionId: sessionId,
       },
       query: {
         chat: sessionId,
       },
     }),
-  })
+  });
   return (
     <>
-      sessionId: {sessionId}
-      <DebugValue data={agStateQuery.data} />
-      {/* <h1>session home : {ssessionId}</h1> */}
+      <DebugValue data={{ data: agStateQuery.data, sessionId }} />
       <ChatClient />
     </>
-  )
+  );
 }
