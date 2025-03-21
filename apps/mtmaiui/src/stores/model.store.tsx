f890682fcca884887e1488ea2d0d586f47594840
an87k287 kk <an87k287@proton.me>
2025-03-21 05:02:02 +0000

    1

--- apps/mtmaiui/src/stores/model.store.tsx
+++ apps/mtmaiui/src/stores/model.store.tsx
@@ -8,6 +8,12 @@ import {
 } from "mtxuilib/ui/sheet";
 
 import type { Gallery } from "mtmaiapi";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "mtxuilib/ui/dialog";
 import { createContext, useContext, useEffect, useMemo } from "react";
 import { type StateCreator, createStore, useStore } from "zustand";
 import { devtools, subscribeWithSelector } from "zustand/middleware";
@@ -19,6 +25,7 @@ export interface ModalProps {
   galleries?: Gallery[];
 }
 
+type ModalVariant = "modal" | "sheet" | "drawer";
 interface ModalState extends ModalProps {
   open: boolean;
   setOpen: (open: boolean) => void;
@@ -26,6 +33,10 @@ interface ModalState extends ModalProps {
   setContent: (children: React.ReactNode) => void;
   header: React.ReactNode;
   setHeader: (children: React.ReactNode) => void;
+  variant: ModalVariant;
+  setVariant: (variant: ModalVariant) => void;
+  children: React.ReactNode;
+  setChildren: (children: React.ReactNode) => void;
 }
 export const createModelStoreSlice: StateCreator<
   ModalState,
@@ -36,8 +47,14 @@ export const createModelStoreSlice: StateCreator<
   return {
     open: false,
     setOpen: (open: boolean) => set({ open }),
-    content: null,
-    setContent: (children: React.ReactNode) => set({ content: children }),
+    // content: null,
+    // setContent: (children: React.ReactNode) => set({ content: children }),
+    variant: "modal",
+    setVariant: (variant: "modal" | "sheet") => set({ variant }),
+    children: null,
+    setChildren: (children: React.ReactNode) => set({ children }),
+    // header: null,
+    // setHeader: (header: React.ReactNode) => set({ header }),
     ...init,
   };
 };
@@ -97,11 +114,19 @@ const ModalDisplay = () => {
   const open = useModelStore((x) => x.open);
   const setOpen = useModelStore((x) => x.setOpen);
   const children = useModelStore((x) => x.content);
+  const variant = useModelStore((x) => x.variant);
 
   const handleClose = () => {
     setOpen(false);
     window.history.back();
   };
+  if (variant === "modal") {
+    return (
+      <Dialog open={open} onOpenChange={handleClose}>
+        {children}
+      </Dialog>
+    );
+  }
   return (
     <Sheet open={open} onOpenChange={handleClose}>
       {children}
@@ -127,6 +152,10 @@ interface ModelHeaderProps {
   children: React.ReactNode;
 }
 export const ModelHeader = ({ children }: ModelHeaderProps) => {
+  const variant = useModelStore((x) => x.variant);
+  if (variant === "modal") {
+    return <DialogHeader>{children}</DialogHeader>;
+  }
   return <SheetHeader>{children}</SheetHeader>;
 };
 
@@ -134,6 +163,10 @@ interface ModelTitleProps {
   children: React.ReactNode;
 }
 export const ModelTitle = ({ children }: ModelTitleProps) => {
+  const variant = useModelStore((x) => x.variant);
+  if (variant === "modal") {
+    return <DialogTitle>{children}</DialogTitle>;
+  }
   return <SheetTitle>{children}</SheetTitle>;
 };
 
@@ -141,6 +174,12 @@ interface ModelContentProps {
   children: React.ReactNode;
 }
 export const ModelContent = ({ children }: ModelContentProps) => {
+  const variant = useModelStore((x) => x.variant);
+  if (variant === "modal") {
+    return (
+      <DialogContent className="w-full sm:max-w-5xl">{children}</DialogContent>
+    );
+  }
   return (
     <SheetContent className="w-full sm:max-w-5xl">{children}</SheetContent>
   );
