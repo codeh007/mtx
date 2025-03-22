8823e3980cb89dfde09866cbe7af6a5728def26e
jksxzzid45 f <jksxzzid45@proton.me>
2025-03-22 09:51:19 +0000

    1

--- apps/mtmaiui/src/routes/~coms/~$comId/~team_builder/~agent/~index.lazy.tsx
+++ apps/mtmaiui/src/routes/~coms/~$comId/~team_builder/~agent/~index.lazy.tsx
@@ -1,8 +1,4 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { PlusIcon } from "lucide-react";
-import { cn } from "mtxuilib/lib/utils";
-import { CustomLink } from "mtxuilib/mt/CustomLink";
-import { buttonVariants } from "mtxuilib/ui/button";
 
 export const Route = createLazyFileRoute("/coms/$comId/team_builder/agent/")({
   component: RouteComponent,
@@ -10,16 +6,5 @@ export const Route = createLazyFileRoute("/coms/$comId/team_builder/agent/")({
 
 function RouteComponent() {
   const { comId } = Route.useParams();
-  return (
-    <>
-      <div className="flex items-center gap-2 justify-end mr-2 pt-2">
-        <CustomLink
-          to={`/coms/${comId}/team_builder/agent/new`}
-          className={cn(buttonVariants({ variant: "outline", size: "icon" }))}
-        >
-          <PlusIcon className="size-4" />
-        </CustomLink>
-      </div>
-    </>
-  );
+  return <></>;
 }
--- apps/mtmaiui/src/routes/~coms/~$comId/~team_builder/~agent/~route.lazy.tsx
+++ apps/mtmaiui/src/routes/~coms/~$comId/~team_builder/~agent/~route.lazy.tsx
@@ -1,7 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { Outlet, createLazyFileRoute } from "@tanstack/react-router";
-import { agentListOptions } from "mtmaiapi";
+import { PlusIcon } from "lucide-react";
+import { type Agent, agentListOptions } from "mtmaiapi";
 import { DebugValue } from "mtxuilib/components/devtools/DebugValue";
+import { cn } from "mtxuilib/lib/utils";
+import { CustomLink } from "mtxuilib/mt/CustomLink";
+import { buttonVariants } from "mtxuilib/ui/button";
 import { useTenantId } from "../../../../../hooks/useAuth";
 
 export const Route = createLazyFileRoute("/coms/$comId/team_builder/agent")({
@@ -23,9 +27,25 @@ function RouteComponent() {
   });
   return (
     <>
-      <div>agent</div>
-      <DebugValue data={agentsQuery.data} />
+      <div className="flex items-center gap-2 justify-end mr-2 pt-2">
+        <CustomLink
+          to={`/coms/${comId}/team_builder/agent/new`}
+          className={cn(buttonVariants({ variant: "outline", size: "icon" }))}
+        >
+          <PlusIcon className="size-4" />
+        </CustomLink>
+        <DebugValue data={agentsQuery.data} />
+      </div>
+      <div className="flex flex-col gap-2">
+        {agentsQuery.data?.rows?.map((agent) => (
+          <AgentItem key={agent.metadata?.id} agent={agent} />
+        ))}
+      </div>
       <Outlet />
     </>
   );
+}
+
+function AgentItem({ agent }: { agent: Agent }) {
+  return <div className="bg-blue-100 p-2 mb-2">{agent.name}</div>;
 }
