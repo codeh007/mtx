124e7adb0b69a734b84ea37f7aaae0e73b2752b7
paicjka87d s <paicjka87d@proton.me>
2025-03-25 09:26:18 +0000

    1

--- apps/mtmaiui/src/routes/~tenant/~settings/sidebar.tsx
+++ apps/mtmaiui/src/routes/~tenant/~settings/sidebar.tsx
@@ -2,11 +2,7 @@
 
 import { Label } from "@radix-ui/react-dropdown-menu";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import {
-  type MtComponent,
-  type TenantSetting,
-  tenantSettingsListOptions,
-} from "mtmaiapi";
+import { type TenantSetting, tenantSettingsListOptions } from "mtmaiapi";
 import { cn, generateUUID } from "mtxuilib/lib/utils";
 import { CustomLink } from "mtxuilib/mt/CustomLink";
 import { buttonVariants } from "mtxuilib/ui/button";
@@ -19,13 +15,14 @@ import {
   SidebarInput,
 } from "mtxuilib/ui/sidebar";
 
+import { Icons } from "mtxuilib/icons/icons";
 import { Switch } from "mtxuilib/ui/switch";
 import { useMemo } from "react";
 import { useTenantId } from "../../../hooks/useAuth";
 
 export function NavTenantSettings() {
   const tid = useTenantId();
-  const comsQuery = useSuspenseQuery({
+  const tenantSettingsQuery = useSuspenseQuery({
     ...tenantSettingsListOptions({
       path: {
         tenant: tid!,
@@ -37,18 +34,18 @@ export function NavTenantSettings() {
     const newUUID = generateUUID();
     return `/tenant/settings/${newUUID}/new`;
   }, []);
-  
+
   return (
     <Sidebar collapsible="none" className="hidden flex-1 md:flex">
       <SidebarHeader className="gap-3.5 border-b p-4">
         <div className="flex w-full items-center justify-between">
-          <div className="text-base font-medium text-foreground">资源</div>
+          <div className="text-base font-medium text-foreground">租户设置</div>
           <Label className="flex items-center gap-2 text-sm">
             <CustomLink
               to={linkToNew}
-              className={cn(buttonVariants({ variant: "ghost" }))}
+              className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}
             >
-              <span>+</span>
+              <Icons.plus className="size-4" />
             </CustomLink>
             <Switch className="shadow-none" />
           </Label>
@@ -58,8 +55,12 @@ export function NavTenantSettings() {
       <SidebarContent>
         <SidebarGroup className="px-0">
           <SidebarGroupContent>
-            {comsQuery.data?.rows?.map((item) => (
-              <NavTenantSettingItem key={item.metadata?.id} item={item} />
+            {tenantSettingsQuery.data?.rows?.map((item) => (
+              <NavTenantSettingItem
+                key={item.metadata?.id}
+                item={item}
+                rowId={item.metadata?.id || ""}
+              />
             ))}
           </SidebarGroupContent>
         </SidebarGroup>
@@ -68,21 +69,10 @@ export function NavTenantSettings() {
   );
 }
 
-const NavTenantSettingItem = ({ item }: { item: TenantSetting }) => {
-  const detailLink = useMemo(() => {
-    // return `${item.metadata?.id}/type/${item.type}`;
-    return `${item.metadata?.id}/view`;
-  }, [item]);
-
-  const team = item;
-  return (
-    <>
-      <NavTeamItem item={team} rowId={item.metadata?.id || ""} />
-    </>
-  );
-};
-
-const NavTeamItem = ({ item, rowId }: { item: MtComponent; rowId: string }) => {
+const NavTenantSettingItem = ({
+  item,
+  rowId,
+}: { item: TenantSetting; rowId: string }) => {
   const detailLink = useMemo(() => {
     // return `${item.metadata?.id}/type/${item.type}`;
     return `${rowId}/view`;
@@ -95,12 +85,12 @@ const NavTeamItem = ({ item, rowId }: { item: MtComponent; rowId: string }) => {
         className="flex flex-col items-start gap-2 whitespace-nowrap border-b p-4 text-sm leading-tight last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
       >
         <div className="flex w-full items-center gap-2">
-          <span>{item.label}</span>
+          <span>{item.metadata.id}</span>
           {/* <span className="ml-auto text-xs">{chat.createdAt}</span> */}
         </div>
-        <span className="font-medium">{item.label}</span>
+        {/* <span className="font-medium">{item.label}</span> */}
         <span className="line-clamp-2 w-[260px] whitespace-break-spaces text-xs">
-          {item.description || rowId}
+          {/* {item.description || rowId} */}
         </span>
       </CustomLink>
     </>
