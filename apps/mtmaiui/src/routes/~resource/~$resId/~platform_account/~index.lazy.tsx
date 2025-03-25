696284a47c01e976ea69a32b6c354055df33e4e8
paicjka87d s <paicjka87d@proton.me>
2025-03-25 05:02:52 +0000

    1

--- apps/mtmaiui/src/routes/~resource/~$resId/~platform_account/~index.lazy.tsx
+++ apps/mtmaiui/src/routes/~resource/~$resId/~platform_account/~index.lazy.tsx
@@ -1,9 +1,83 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { useMutation } from "@tanstack/react-query";
+import { Outlet, createLazyFileRoute } from "@tanstack/react-router";
+import { resourceUpsertMutation } from "mtmaiapi";
+import { zMtResourceUpsert } from "mtmaiapi/gomtmapi/zod.gen";
+import { EditFormToolbar } from "mtxuilib/mt/form/EditFormToolbar";
+import { ZForm, useZodForm } from "mtxuilib/mt/form/ZodForm";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "mtxuilib/ui/form";
+import { Input } from "mtxuilib/ui/input";
+import { useTenantId } from "../../../../hooks/useAuth";
 
 export const Route = createLazyFileRoute("/resource/$resId/platform_account/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>账号管理</div>;
+  const tid = useTenantId();
+  const resourceUpsert = useMutation({
+    ...resourceUpsertMutation(),
+  });
+  const form = useZodForm({
+    schema: zMtResourceUpsert,
+    defaultValues: {},
+  });
+  return (
+    <div>
+      账号管理
+      <ZForm
+        form={form}
+        handleSubmit={(values) => {
+          console.log(values);
+          resourceUpsert.mutate({
+            path: {
+              tenant: tid,
+            },
+            body: {
+              ...values,
+            },
+          });
+        }}
+        className="space-y-2"
+      >
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <input type="hidden" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>title</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="title" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Outlet />
+        <EditFormToolbar form={form} />
+        {form.formState.errors && (
+          <div className="text-red-500">
+            {JSON.stringify(form.formState.errors, null, 2)}
+          </div>
+        )}
+      </ZForm>
+    </div>
+  );
 }
--- apps/mtmaiui/src/routes/~resource/~index.lazy.tsx
+++ apps/mtmaiui/src/routes/~resource/~index.lazy.tsx
@@ -1,6 +1,6 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { type MtResource, resourceListOptions } from "mtmaiapi";
+import { generateUUID } from "mtxuilib/lib/utils";
+import { CustomLink } from "mtxuilib/mt/CustomLink";
 import { useTenantId } from "../../hooks/useAuth";
 
 export const Route = createLazyFileRoute("/resource/")({
@@ -9,24 +9,25 @@ export const Route = createLazyFileRoute("/resource/")({
 
 function RouteComponent() {
   const tid = useTenantId();
-  const query = useSuspenseQuery({
-    ...resourceListOptions({
-      path: {
-        tenant: tid,
-      },
-    }),
-  });
+  // const query = useSuspenseQuery({
+  //   ...resourceListOptions({
+  //     path: {
+  //       tenant: tid,
+  //     },
+  //   }),
+  // });
+
+  const newId = generateUUID();
   return (
     <div className="flex flex-col h-full w-full p-2">
+      <CustomLink to={`/resource/${newId}/platform_account`}>
+        新建ig账号
+      </CustomLink>
       <div>
-        {query.data?.rows?.map((item) => (
+        {/* {query.data?.rows?.map((item) => (
           <ResourceListItem key={item.metadata?.id} item={item} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
-}
-
-function ResourceListItem({ item }: { item: MtResource }) {
-  return <div className="flex bg-slate-50 p-2">{item.metadata?.id}</div>;
 }
