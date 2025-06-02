e91f414355d1ebc63f8a79bf06cb84f5dff59a2f
a <a@a.com>
2025-06-02 12:31:09 +0000

    1

--- apps/mtmaiui/src/components/webview/HelloWebviewContent.tsx
+++ apps/mtmaiui/src/components/webview/HelloWebviewContent.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
 
-export default function MtmJsTest() {
-  useEffect(() => {
-    console.log("MtmJsTest");
-  }, []);
-  return <div>
+export function HelloWebviewContent() {
+  
+  return <div className="flex flex-col min-h-screen h-full w-full bg-red-200">
     <h2 className="text-2xl font-bold bg-red-500">MtmJsTest</h2>
-
-
     <a href="https://www.bing.com" className="text-blue-500">Bing</a>
   </div>;
 }
